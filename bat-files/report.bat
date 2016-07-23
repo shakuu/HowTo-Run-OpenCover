1db23f74a4b8d7bcc1002eeab747f1ca646cf67f
shakuu <todorov.h@gmail.com>
2016-07-23 13:52:35 +0300

    runtests

--- bat-files/report.bat
+++ bat-files/report.bat
@@ -1,2 +1,2 @@
-..\\..\\..\\packages\\OpenCover.4.6.519\\tools\\OpenCover.Console.exe -target:tests.bat -register:user
+..\\..\\..\\packages\\OpenCover.4.6.519\\tools\\OpenCover.Console.exe -target:runtests.bat -register:user
 ..\\..\\..\\packages\\ReportGenerator.2.4.5.0\\tools\\ReportGenerator.exe -reports:results.xml -targetdir:coverage
