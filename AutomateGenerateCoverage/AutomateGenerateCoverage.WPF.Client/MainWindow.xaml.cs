22b65f25a9fee82c8297af3f6abafeb5e7c72ef2
shakuu <todorov.h@gmail.com>
2016-07-24 02:01:21 +0300

    NEW: WPF Client

---
+++ AutomateGenerateCoverage/AutomateGenerateCoverage.WPF.Client/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace AutomateGenerateCoverage.WPF.Client
+{
+    using Microsoft.Win32;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Startup : Window
+    {
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectTargetTestingLibraryBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var myDialog = new OpenFileDialog();
+
+            myDialog.ShowDialog();
+
+            SelectedFileTextBox.Text = myDialog.FileName;
+        }
+    }
+}
