29ee89a170e3dcd8314a610cc33e5d4117c88764
shakuu <todorov.h@gmail.com>
2016-07-24 07:34:23 +0300

    cleaning up

--- AutomateGenerateCoverage/AutomateGenerateCoverage.WPF.Client/MainWindow.xaml.cs
+++ AutomateGenerateCoverage/AutomateGenerateCoverage.WPF.Client/MainWindow.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-
-namespace AutomateGenerateCoverage.WPF.Client
+﻿namespace AutomateGenerateCoverage.WPF.Client
 {
     using Microsoft.Win32;
     using System.Diagnostics;
+    using System;
+    using System.Windows;
 
     using AutomateGenerateCoverage.Contracts.Reports;
     using AutomateGenerateCoverage.Models.Reports;
